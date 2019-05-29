56df2210fc7e4d48a792a5e965de06048ac7a463
majun <598959594@qq.com>
2019-05-30 00:19:05 +0800

    ioutil

---
+++ ioPackage/ioutilPackage/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+)
+
+func main() {
+	// rwx 421 user group other
+	err := ioutil.WriteFile("current.bak", []byte("testwrite"), 0644)
+	CheckErr(err)
+
+	/*
+		直接读取文件内容,参数是文件名
+	*/
+	content2, err := ioutil.ReadFile("current.bak")
+	CheckErr(err)
+	fmt.Println(string(content2))
+
+	/*
+		创建临时目录, "" 表示 /tmp
+	*/
+	dirname, err := ioutil.TempDir("", "example")
+	CheckErr(err)
+	fmt.Println(dirname) // /tmp/example331421827
+	err = ioutil.WriteFile(filepath.Join(dirname, "test.txt"), []byte("memgnlima\n"), 0644)
+	CheckErr(err)
+
+	/*
+		创建临时文件
+	*/
+	tmpfile, err := ioutil.TempFile("", "file.txt")
+	fmt.Println(tmpfile.Name())
+	_, err = tmpfile.Write([]byte("menglima"))
+	CheckErr(err)
+
+}
+
+func CheckErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
