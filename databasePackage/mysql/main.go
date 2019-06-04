2ac0c5f9f7f81e1e37a7bc01b09f66dcdcf9d503
majun <majun>
2019-06-04 11:32:52 +0800

    sql tx

--- databasePackage/mysql/main.go
+++ databasePackage/mysql/main.go
@@ -7,8 +7,9 @@ import (
 )
 
 func main() {
-	db, err := sql.Open("mysql", "root:123456@tcp(localhost:3306)/test?charset=utf8")
+	db, err := sql.Open("mysql", "root:123456@tcp(localhost:3306)/test_go?charset=utf8")
 	defer db.Close()
+	CheckErr(err)
 }
 func CheckErr(err error) {
 	if err != nil {
--- databasePackage/sqlite/mainMemery.go
+++ databasePackage/sqlite/mainMemery.go
@@ -8,13 +8,51 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// https://www.jianshu.com/p/bc8120bec94e
+
 /*
+	prepare 的设计初衷是多次执行:
+		可以实现自定义参数
+		比手动拼接 sql 语句更高效
+		可以防止 SQL 注入攻击
+
 	事务:
 	tx, err := db.Begin()
+
 	tx.Prepare
 	stmt.Exec
-		defer stmt.Close()
-	tx.Commit()
+		defer stmt.Close()  中间这三个步骤有点小问题，因为 commit 或 rollback 后，连接就释放了，不需要再进行 close，因此在事物中尽量避免 prepare
+
+		tx.Commit()
+	defer tx.RollBack()
+
+	一旦创建了 tx 对象，事物的处理都依赖于 tx 对象，该对象会从连接池中取出一个空闲的连接，
+	接下来的所有操作都基于该连接，直到 commit 或 rollback 后才会将连接释放至连接池；
+	事物过程中只有一个连接；
+	事物内的操作都是顺序执行；
+	普通的 db 执行一个操作就是一个连接；
+
+
+	tx 的并发问题：
+		对于sql.Tx对象，因为事务过程只有一个连接，事务内的操作都是顺序执行的，在开始下一个数据库交互之前，必须先完成上一个数据库交互。例如下面的例子：
+		rows, _ := db.Query("SELECT id FROM user")
+		for rows.Next() {
+			var mid, did int
+			rows.Scan(&mid)
+			db.QueryRow("SELECT id FROM detail_user WHERE master = ?", mid).Scan(&did)
+
+		}
+
+		调用了Query方法之后，在Next方法中取结果的时候，rows是维护了一个连接，再次调用QueryRow的时候，db会再从连接池取出一个新的连接。rows和db的连接两者可以并存，并且相互不影响。
+		可是，这样逻辑在事务处理中将会失效：
+		rows, _ := tx.Query("SELECT id FROM user")
+		for rows.Next() {
+		var mid, did int
+		rows.Scan(&mid)
+		tx.QueryRow("SELECT id FROM detail_user WHERE master = ?", mid).Scan(&did)
+		}
+
+		tx执行了Query方法后，连接转移到rows上，在Next方法中，tx.QueryRow将尝试获取该连接进行数据库操作。因为还没有调用rows.Close，因此底层的连接属于busy状态，tx是无法再进行查询的。上面的例子看起来有点傻，毕竟涉及这样的操作，使用query的join语句就能规避这个问题。例子只是为了说明tx的使用问题。
 */
 
 type BCCode struct {
@@ -63,8 +101,21 @@ func main() {
 		_, err := stmt.Exec(fmt.Sprintf("B%024d", i), fmt.Sprintf("B%024d", i), 0, 1)
 		CheckErr1(err)
 	}
-	tx.Commit()
 	end := time.Now().Unix()
+
+	/*
+		用来实现提交或回滚，这里的 recover 只针对事物里面的 err(示例代码)
+		为了实现 err 不被覆盖，还可以进行检测，当 err 不为 nil 的时候，return，然后调用 defer 就行，也不需要 recover 了；
+	*/
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println(r)
+			tx.Rollback()
+		} else {
+			tx.Commit()
+			fmt.Println("ok")
+		}
+	}()
 
 	// 随机检索 10 万次
 	var count int = 0
@@ -77,6 +128,7 @@ func main() {
 		CheckErr1(err)
 		count++
 	}
+
 	queryEnd := time.Now().Unix()
 	fmt.Println("insert into 10万次,cost:", float64(end)-float64(start))
 	fmt.Println("query 10万次,cost:", float64(queryEnd)-float64(end))
