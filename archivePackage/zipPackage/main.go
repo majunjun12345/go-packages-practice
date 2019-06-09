a273e7bf7516990c35145a951ad29e4a05974b03
majun <majun>
2019-06-10 00:24:34 +0800

    zip

--- archivePackage/zipPackage/main.go
+++ archivePackage/zipPackage/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"archive/zip"
-	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 )
@@ -54,30 +52,24 @@ func UnZip(archieveFilePath, target string) error {
 }
 
 func ToZip() {
-	// buf := new(bytes.Buffer)
-	// buf.
-	// w := zip.NewWriter(buf)
 	fi, err := os.OpenFile("test.tar", os.O_CREATE|os.O_WRONLY, 0666)
-	defer fi.Close()
 	ChectErr(err)
 	w := zip.NewWriter(fi)
 	defer w.Close()
 
-	err1 := filepath.Walk("./manageTool", func(path string, info os.FileInfo, err error) error {
+	err1 := filepath.Walk("manageTool", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			fmt.Println(err)
-			panic(err)
 			return err
 		}
-		f, err := w.Create(path)
 
-		if err != nil {
-			panic(err)
-		}
-		content, err := ioutil.ReadFile(path)
-		_, err = f.Write(content)
-		if err != nil {
-			panic(err)
+		if !info.IsDir() { // 归档的时候，忽略目录，归档文件的适合自动创建目录
+			file, err := os.Open(path)
+			writer, err := w.Create(path) // 使用 CreateHeader 的目的是自定义 name 和 Method
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			_, err = io.Copy(writer, file)
 		}
 		return err
 	})
