110e3f65adc212d484733963bce1b734c12fef36
majun <598959594@qq.com>
2019-06-12 09:08:45 +0800

    time

--- timePackage/main.go
+++ timePackage/main.go
@@ -28,15 +28,51 @@ func UnixTime() {
 	时间格式化字符串转换
 */
 func TimeFormat() {
-	const longForm = "Jan 2, 2006 at 3:04pm (MST)"
-	t, _ := time.Parse(longForm, "Jun 21, 2017 at 0:00am (PST)") // 2017-06-21 00:00:00 +0000 PST
-	fmt.Println(t)
 
-	const shortForm = "2006-Jan-02"
-	t, _ = time.Parse(shortForm, "2017-Jun-21") // 2017-06-21 00:00:00 +0000 UTC
-	fmt.Println(t)
+	// ticker,每隔一段时间执行任务
 
-	t, _ = time.Parse("01/02/2006", "06/21/2017")
-	fmt.Println(t)
-	fmt.Println(t.Unix())
+	// timer　指定时间执行
+	TimeChan()
+}
+
+func t() {
+	now := time.Now()
+	fmt.Println(now.Year())           // 2019
+	fmt.Println(now.Month().String()) // June
+	fmt.Println(now.Day())            // 12
+	fmt.Println(now.Date())           // 2019 June 12
+	fmt.Println(now.Hour())           // 8
+	fmt.Println(now.Minute())         // 16
+	fmt.Println(now.Second())         // 31
+
+	// format
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05")) // 2019-06-12 08:20:12
+	fmt.Println(time.Now().Format("2006-01-02#15:04:05")) // 2019-06-12#08:20:12
+	fmt.Println(time.Now().Format("15:04:05"))            // 08:20:12
+}
+
+func TimeChan() {
+	timeOut := time.After(time.Second * 10)
+	ticker := time.Tick(time.Second * 2)
+	// timer := time.NewTicker(time.Second * 10).C
+
+	s := make(chan int)
+
+	go func() {
+		for {
+			select {
+			case <-s:
+				fmt.Println("receive signal, stop!")
+			case <-ticker:
+				fmt.Println("tick")
+			case <-timeOut:
+				fmt.Println("time out")
+			}
+		}
+	}()
+
+	time.Sleep(time.Second * 15)
+	s <- 3
+	time.Sleep(time.Second * 2)
+	fmt.Println("ending")
 }
