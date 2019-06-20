74b48101aa1d870a2292707be3d77c149aeb23e6
majun <598959594@qq.com>
2019-06-20 21:55:38 +0800

    mgo page

--- dbPackage/mgoPackage/main.go
+++ dbPackage/mgoPackage/main.go
@@ -7,14 +7,34 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+/*
+	go 中大写开头的字段,在 mgo 中都变成了小写, 除非有标签
+	分页:https://segmentfault.com/a/1190000019437349?utm_source=tag-newest
+*/
+
 var session mgo.Session
 var db *mgo.Database
 var c *mgo.Collection
 
 func init() {
 	session, err := mgo.Dial("localhost:27017")
+	// mgo.DialWithTimeout
+	// mgo.DialWithInfo() 可以设置用户名 密码 连接池(默认是 4096) 超时 数据库等
 	db = session.DB("testGoMgo")
-	session.SetMode(mgo.Monotonic, true)
+	/*
+		结论:
+			strong 可靠性最强, eventual 对读写分离性能的提升最强
+
+		Strong(默认)
+			session 的读写一直向主服务器发起并使用一个唯一的连接，因此所有的读写操作完全的一致。
+		Monotonic
+			session 的读操作开始是向其他服务器发起（且通过一个唯一的连接），只要出现了一次写操作，session 的连接就会切换至主服务器。
+			由此可见此模式下，能够分散一些读操作到其他服务器，但是读操作不一定能够获得最新的数据。
+		Eventual
+			session 的读操作会向任意的其他服务器发起，多次读操作并不一定使用相同的连接，也就是读操作不一定有序。
+			session 的写操作总是向主服务器发起，但是可能使用不同的连接，也就是写操作也不一定有序。
+	*/
+	session.SetMode(mgo.Monotonic, true) // 设置 mongo 的读写模式
 	// defer session.Close()   // 不能这样，执行 init 后就关闭了
 	CheckErr(err)
 	c = db.C("people")
@@ -22,30 +42,71 @@ func init() {
 
 func main() {
 	// mgoInsert()
-	mgoSearch()
+
+	// mgoSearch()
+
+	mgoPage()
+
+	// mgoUpdate()
+
+	// mgoDel()
 }
 
 type Person struct {
-	// Id_   bson.ObjectId `bson:"_id"`
-	Name  string
-	Phone string
-	Age   int
+	ID    bson.ObjectId `bson:"_id"`
+	Name  string        `bson:"name"`
+	Phone string        `bson:"phone"`
+	Age   int           `bson:"age"`
 }
 
 func mgoInsert() {
-	err := c.Insert(Person{Name: "menglima", Phone: "189", Age: 21}, Person{Name: "mamengli", Phone: "176", Age: 19})
+	err := c.Insert(&Person{ID: bson.NewObjectId(), Name: "menglima", Phone: "189", Age: 99}, &Person{ID: bson.NewObjectId(), Name: "mamengli", Phone: "176", Age: 99})
 	CheckErr(err)
 }
 
 func mgoSearch() {
 
+	// one
 	result := Person{}
 	// err = c.Find(bson.M{"name": "menglima"}).One(&result)
 	// err = c.FindId(bson.M{"_id": 12}).One(&result)
-	res := []Person{}
-	err := c.Find(bson.M{"Name": bson.M{"$regex": "A", "$options": "$i"}}).All(&res) // $regex 正则匹配, "$options": "$i" 忽略大小写
+	err := c.Find(bson.M{"age": bson.M{"$lt": 20}}).One(&result) // lt:<, gt:>, 加上 e 就有了 =
 	CheckErr(err)
 	fmt.Printf("%+v\n", result)
+
+	// all
+	// res := []Person{}
+	// err = c.Find(bson.M{"name": bson.M{"$regex": "A", "$options": "$i"}}).All(&res) // $regex 正则匹配, "$options": "$i" 忽略大小写, name 的 n 必须是小写
+	// CheckErr(err)
+	// fmt.Printf("%+v\n", res)
+}
+
+// -----------------分页
+func mgoPage() {
+	res := []Person{}
+	c.Find(bson.M{"name": "mamengli"}).Select(bson.M{"age": 22}).Skip(0).Limit(10).All(&res)
+	fmt.Printf("%+v", res)
+}
+
+func mgoUpdate() {
+	p := Person{bson.NewObjectId(), "zhangsan", "111", 15}
+	err := c.Update(bson.M{"name": "menglima"}, p)
+	CheckErr(err)
+
+	// 批量更新和只更新一条格式不一样
+	updater := bson.M{"$set": bson.M{"age": 22}}
+	info, err := c.UpdateAll(bson.M{"age": 22}, updater)
+	CheckErr(err)
+	fmt.Println(info.Updated)
+}
+
+func mgoDel() {
+	err := c.Remove(bson.M{"age": 15})
+	CheckErr(err)
+
+	info, err := c.RemoveAll(bson.M{"age": 21})
+	CheckErr(err)
+	fmt.Println(info.Removed)
 }
 
 func CheckErr(err error) {
