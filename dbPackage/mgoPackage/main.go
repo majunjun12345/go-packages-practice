6815ca8c303ae5616634ad1a11d8720e819ba55b
majun <majun>
2019-06-20 19:09:43 +0800

    mgo

--- dbPackage/mgoPackage/main.go
+++ dbPackage/mgoPackage/main.go
@@ -14,23 +14,26 @@ var c *mgo.Collection
 func init() {
 	session, err := mgo.Dial("localhost:27017")
 	db = session.DB("testGoMgo")
-	defer session.Clone()
+	session.SetMode(mgo.Monotonic, true)
+	// defer session.Close()   // 不能这样，执行 init 后就关闭了
 	CheckErr(err)
 	c = db.C("people")
 }
 
 func main() {
+	// mgoInsert()
 	mgoSearch()
 }
 
 type Person struct {
+	// Id_   bson.ObjectId `bson:"_id"`
 	Name  string
 	Phone string
 	Age   int
 }
 
 func mgoInsert() {
-	err := c.Insert(&Person{"menglima", "189", 21}, &Person{"mamengli", "176", 19})
+	err := c.Insert(Person{Name: "menglima", Phone: "189", Age: 21}, Person{Name: "mamengli", Phone: "176", Age: 19})
 	CheckErr(err)
 }
 
@@ -40,7 +43,7 @@ func mgoSearch() {
 	// err = c.Find(bson.M{"name": "menglima"}).One(&result)
 	// err = c.FindId(bson.M{"_id": 12}).One(&result)
 	res := []Person{}
-	err := c.Find(bson.M{"name": bson.M{"$regex": "A", "$options": "$i"}}).All(&res) // $regex 正则匹配, "$options": "$i" 忽略大小写
+	err := c.Find(bson.M{"Name": bson.M{"$regex": "A", "$options": "$i"}}).All(&res) // $regex 正则匹配, "$options": "$i" 忽略大小写
 	CheckErr(err)
 	fmt.Printf("%+v\n", result)
 }
