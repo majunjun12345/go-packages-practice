d85acf2e7d5bc1da570950218c7366a21677605b
majun <598959594@qq.com>
2019-06-27 23:48:54 +0800

    websocket

--- websocketPackage/server/server.go
+++ websocketPackage/server/server.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	id     string
+	socket *websocket.Conn
+	send   chan []byte
+}
+
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Message struct {
+	Sender    string `json:"sender, omitempty"`
+	Recipient string `json:"recipient, omitempty"`
+	Content   string `json:"content, omitempty"`
+}
+
+var manager = ClientManager{
+	broadcast:  make(chan []byte),
+	register:   make(chan *Client),
+	unregister: make(chan *Client),
+	clients:    make(map[*Client]bool),
+}
+
+func (cm *ClientManager) start() {
+	for {
+		select {
+		case conn := <-cm.register:
+			cm.clients[conn] = true
+
+		}
+	}
+}
+
 func main() {
 
 }
