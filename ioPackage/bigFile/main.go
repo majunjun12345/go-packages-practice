dc4e70bd14079523b730ad0863ce32a31c912270
majun <598959594@qq.com>
2019-07-15 09:08:18 +0800

    bigfile read

---
+++ ioPackage/bigFile/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"io"
+	
+	"fmt"
+	"os"
+	"bufio"
+)
+
+/*
+	打开文件不会耗费资源,打开文件只是使用文件句柄,并不会将文件导入内存
+*/
+
+func main() {
+	
+}
+
+// 流式读取文本,因为有换行符
+func StreamRead()  {
+	f, err := os.Open("filename")
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	scan := bufio.NewScanner(f)
+	scan.Split(bufio.ScanLines)
+
+	for scan.Scan(){
+		fmt.Println(scan.Text())
+	}
+}
+
+// 分片处理,针对二进制文件
+func SliceRead()  {
+	f, err := os.Open("filename")
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	s := make([]byte, 4096)
+	for {
+		_, err := f.Read(s)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+		fmt.Fprintln(os.Stdout, s)
+	}
+}
