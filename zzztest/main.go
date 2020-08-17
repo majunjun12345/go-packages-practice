b77e17eb71b6d16252d99c5c7c0f8a2c9ed7ead9
majunjun12345 <598959594@qq.com>
2020-08-17 14:31:33 +0800

    fix: zzz:custom zzztest:main

---
+++ zzztest/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	_ "net/http/pprof"
+)
+
+func funcA() []byte {
+	a := make([]byte, 10*1024*1024)
+	return a
+}
+
+func funcB() ([]byte, []byte) {
+	a := make([]byte, 10*1024*1024)
+	b := funcA()
+	return a, b
+}
+
+func funcC() ([]byte, []byte, []byte) {
+	a := make([]byte, 10*1024*1024)
+	b, c := funcB()
+	return a, b, c
+}
+
+func main() {
+	for i := 0; i < 5; i++ {
+		funcA()
+		funcB()
+		funcC()
+	}
+
+	http.ListenAndServe("0.0.0.0:9999", nil)
+}
