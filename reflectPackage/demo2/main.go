2974c29f69d91ebbc6d01472ef6fbbf7c189ed5a
majunjun12345 <598959594@qq.com>
2020-11-20 13:32:16 +0800

    feat: reflect

--- reflectPackage/demo2/main.go
+++ reflectPackage/demo2/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	// register driver
 	"database/sql"
+	"errors"
+	"fmt"
 	"reflect"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -52,6 +55,7 @@ func Table(db *sql.DB, tableName string) func() *Query {
 func (q *Query) Insert(in interface{}) (int64, error) {
 	var keys, values []string
 	v := reflect.ValueOf(in)
+	t := reflect.TypeOf(in)
 	// 剥离指针
 	for v.Kind() == reflect.Ptr {
 		v = v.Elem() // &User{} 变成 User{}
@@ -59,18 +63,42 @@ func (q *Query) Insert(in interface{}) (int64, error) {
 
 	switch v.Kind() {
 	case reflect.Struct:
+		keys, values = sKV(v)
 	case reflect.Map:
+		keys, values = mKV(v)
 	case reflect.Slice:
+		for i := 0; i < v.Len(); i++ {
+			//Kind是切片时，可以用Index()方法遍历
+			sv := v.Index(i)
+			for sv.Kind() == reflect.Ptr || sv.Kind() == reflect.Interface {
+				sv = sv.Elem()
+			}
+			//切片元素不是struct或者指针，报错
+			if sv.Kind() != reflect.Struct {
+				return 0, errors.New("method Insert error: in slice is not structs")
+			}
+			//keys只保存一次就行，因为后面的都一样了
+			if len(keys) == 0 {
+				keys, values = sKV(sv)
+				continue
+			}
+			_, val := sKV(sv)
+			values = append(values, val...)
+		}
+	default:
+		return 0, errors.New("method Insert error: type error")
 	}
+
+	// TODO:
 	return 0, nil
 }
 
 // sKV struct
-func sKV(v reflect.Value) {
+func sKV(v reflect.Value) ([]string, []string) {
 	var keys, values []string
 	t := v.Type()
 	for i := 0; i < t.NumField(); i++ {
-		tf := t.Field(i)
+		tf := t.Field(i) // TODO: 这两个方法的区别
 		vf := v.Field(i)
 
 		// 忽略非导出字段
@@ -83,8 +111,64 @@ func sKV(v reflect.Value) {
 			continue
 		}
 
+		// 有时候根据需求会组合struct，这里处理下，支持获取嵌套的struct tag和value
+		// 如果字段值是time类型之外的struct，递归获取keys和values
 		if vf.Kind() == reflect.Struct && tf.Type.Name() == "Time" {
+			cKeys, cValues := sKV(vf)
+			keys = append(keys, cKeys...)
+			values = append(values, cValues...)
+			continue
+		}
 
+		// 根据字段的 json tag 获取 key，忽略无 tag 字段
+		key := strings.Split(tf.Tag.Get("json"), ",")[0]
+		if key == "" {
+			continue
+		}
+		value := formatToString(vf)
+		if value != "" {
+			keys = append(keys, key)
+			values = append(values, value)
 		}
 	}
+	return keys, values
+}
+
+func formatToString(v reflect.Value) string {
+	// 断言出 time 类型，直接转 unix 时间戳
+	if t, ok := v.Interface().(time.Time); ok {
+		return fmt.Sprintf("FROM_UNIXTIME(%d)", t.Unix())
+	}
+	switch v.Kind() {
+	case reflect.String:
+		return fmt.Sprintf(`'%s'`, v.String())
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
+		return fmt.Sprintf(`%d`, v.Interface())
+	case reflect.Float32, reflect.Float64:
+		return fmt.Sprintf(`%f`, v.Interface())
+	// 如果是切片类型，遍历元素，递归格式化成"(, , , )"形式
+	case reflect.Slice:
+		var values []string
+		for i := 0; i < v.Len(); i++ {
+			values = append(values, formatToString(v.Index(i)))
+		}
+		return fmt.Sprintf(`(%s)`, strings.Join(values, ","))
+	// 接口类型剥一层递归
+	case reflect.Interface:
+		return formatToString(v.Elem())
+	}
+	return ""
+}
+
+func mKV(v reflect.Value) ([]string, []string) {
+	var keys, values []string
+	mapKeys := v.MapKeys()
+	for _, k := range mapKeys {
+		value := formatToString(v.MapIndex(k))
+		if value != "" {
+			values = append(values, value)
+			keys = append(keys, k.Interface().(string))
+		}
+	}
+	return keys, values
 }
--- reflectPackage/main.go
+++ reflectPackage/main.go
@@ -13,8 +13,8 @@ import (
 */
 
 type User struct {
-	Name    string `gogo:"name"`
-	Age     int    `gogo:"int"`
+	Name    string `gogo:"name" json:"name" id:"100"`
+	Age     int    `gogo:"age"`
 	Married bool   `married:"married"`
 }
 
@@ -24,22 +24,22 @@ func (u *User) Hello() string {
 
 func main() {
 
-	u := &User{"mengliam", 21, false}
-	TStruct(u)
+	// u := &User{"mengliam", 21, false}
+	// TStructType(u)
 
-	// main1()
+	// TStructValue(u)
+
+	TStructCall()
 }
 
-func TStruct(s interface{}) {
-	// Value、Type、kind
-	// Type 表示 interface{} 的实际类型，Kind 表示特别类型
-	v := reflect.ValueOf(s) // &{mengliam 21 false}
-	t := reflect.TypeOf(s)  // main.User
-	k := t.Kind()           // 如果传入的是指针: ptr，如果传入的是结构体: struct
-	fmt.Println("typeof valueof kind", t, v, k)
+// TStructType 通过反射获取类型信息
+func TStructType(s interface{}) {
+	t := reflect.TypeOf(s) // main.User
+	k := t.Kind()          // 如果传入的是指针: ptr，如果传入的是结构体: struct
+	fmt.Println("typeof valueof kind", t, k)
 
 	// 获取结构体字段
-	switch reflect.TypeOf(s).Kind() {
+	switch t.Kind() {
 	case reflect.Struct:
 		for i := 0; i < t.NumField(); i++ {
 			/*
@@ -47,8 +47,124 @@ func TStruct(s interface{}) {
 				Age int 21
 				Married bool false
 			*/
-			fmt.Println(t.Field(i).Name, t.Field(i).Type, v.Field(i).Interface())
+			fmt.Println(t.Field(i).Name, t.Field(i).Type)
 
+			// reflect.StructField Name name  0 [0] false
+			fmt.Println("reflect.StructField", t.Field(i).Name, t.Field(i).Tag.Get("gogo"), t.Field(i).PkgPath, t.Field(i).Offset, t.Field(i).Index, t.Field(i).Anonymous)
+
+			switch t.Field(i).Type.Kind() {
+			case reflect.String:
+				fmt.Println(t.Field(i).Name)
+			case reflect.Int:
+				fmt.Println(t.Field(i).Name)
+			case reflect.Bool:
+				fmt.Println(t.Field(i).Name)
+			default:
+				fmt.Println("Unsupported type", t.Field(i).Name)
+			}
+		}
+
+		// 获取方法， 方法必须是可导出的
+		for i := 0; i < t.NumMethod(); i++ {
+			m := t.Method(i)
+			fmt.Println("=======", m.Name)
+		}
+
+		if m, ok := t.MethodByName("Hello"); ok {
+			/*
+				在Go的指针知识中，有一条规则：一个指针类型拥有它以及它的基底类型为接收者类型的所有方法，而它的基底类型却只能拥有以它本身为接收者类型的方法。
+			*/
+			in := []reflect.Value{reflect.ValueOf(&User{Name: "mamama"})}
+			rs := m.Func.Call(in)
+			fmt.Println("result:", rs[0].Interface().(string))
+		}
+	case reflect.Ptr: // 如果是指针，则要剥离指针
+		fmt.Println("is ptr")
+
+		// 方法
+		{
+			// 获取方法， 方法必须是可导出的
+			for i := 0; i < t.NumMethod(); i++ {
+				m := t.Method(i)
+				fmt.Println("=======", m.Name)
+			}
+
+			if m, ok := t.MethodByName("Hello"); ok {
+				/*
+					User类型是*User的基底类型
+					在Go的指针知识中，有一条规则：一个指针类型拥有它以及它的基底类型为接收者类型的所有方法，而它的基底类型却只能拥有以它本身为接收者类型的方法。
+					如果被剥离指针, 这里将 panic
+				*/
+				in := []reflect.Value{reflect.ValueOf(&User{Name: "mamama"})}
+				rs := m.Func.Call(in)
+				fmt.Println("result:", rs[0].Interface().(string))
+			}
+		}
+
+		// 剥离指针
+		for t.Kind() == reflect.Ptr {
+			t = t.Elem()
+		}
+
+		// 具体字段
+		for i := 0; i < t.NumField(); i++ {
+
+			// Name string
+			fmt.Println("name, type", t.Field(i).Name, t.Field(i).Type.String())
+
+			// reflect.StructField Name name  0 [0] false
+			fmt.Println("reflect.StructField", t.Field(i).Name, t.Field(i).Tag.Get("gogo"), t.Field(i).PkgPath, t.Field(i).Offset, t.Field(i).Index, t.Field(i).Anonymous)
+
+			switch t.Field(i).Type.Kind() {
+			case reflect.String:
+				fmt.Println(t.Field(i).Name)
+			case reflect.Int:
+				fmt.Println(t.Field(i).Name)
+			case reflect.Bool:
+				fmt.Println(t.Field(i).Name)
+			default:
+				fmt.Println("Unsupported type", t.Field(i).Name)
+			}
+		}
+
+		// 通过字段名, 找到字段类型信息
+		if nameType, ok := t.FieldByName("Name"); ok {
+			// 从tag中取出需要的tag
+			fmt.Println(nameType.Tag.Get("json"), nameType.Tag.Get("id"))
+		}
+	}
+}
+
+// TStructValue 通过反射获取值信息
+func TStructValue(s interface{}) {
+	// 判断反射值得空和有效性
+	{
+
+		var a *int
+		fmt.Println("var a *int:", reflect.ValueOf(a).IsNil()) //*int的空指针
+		fmt.Println("nil:", reflect.ValueOf(nil).IsValid())    //nil值
+
+		//实例化一个结构体
+		s := struct{}{}
+		fmt.Println("不存在的结构体成员:", reflect.ValueOf(s).FieldByName("").IsValid()) //尝试从结构体中查找一个不存在的字段
+		fmt.Println("不存在的方法:", reflect.ValueOf(s).MethodByName("").IsValid())   //尝试从结构体中查找一个不存在的方法
+
+		//实例化一个map
+		m := map[int]int{}
+		fmt.Println("不存在的键:", reflect.ValueOf(m).MapIndex(reflect.ValueOf(3)).IsValid()) //尝试从map中查找一个不存在的键
+	}
+
+	v := reflect.ValueOf(s) // &{mengliam 21 false}
+
+	i := v.Interface() // 转换成 interface 类型，可以用来类型断言
+	if vv, ok := i.(*User); ok {
+		fmt.Println("======", vv)
+	}
+
+	// 通过反射访问结构体成员的值
+	switch v.Kind() {
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
 			switch v.Field(i).Kind() {
 			case reflect.String:
 				fmt.Println(v.Field(i).String())
@@ -57,7 +173,7 @@ func TStruct(s interface{}) {
 			case reflect.Bool:
 				fmt.Println(v.Field(i).Bool())
 			default:
-				fmt.Println("Unsupported type", t.Field(i).Name, t.Field(i).Type, v.Field(i).Interface())
+				fmt.Println("Unsupported type", v.Field(i).Interface())
 			}
 		}
 	case reflect.Ptr: // 如果是指针，则要剥离指针
@@ -65,6 +181,9 @@ func TStruct(s interface{}) {
 		for v.Kind() == reflect.Ptr {
 			v = v.Elem()
 		}
+
+		rv := v.FieldByName("Name")
+		fmt.Println("=======", rv.Type().Name())
 
 		for i := 0; i < v.NumField(); i++ {
 			switch v.Field(i).Kind() {
@@ -75,10 +194,11 @@ func TStruct(s interface{}) {
 			case reflect.Bool:
 				fmt.Println(v.Field(i).Bool())
 			default:
-				fmt.Println("Unsupported type", t.Field(i).Name, t.Field(i).Type, v.Field(i).Interface())
+				fmt.Println("Unsupported type", v.Field(i).Interface())
 			}
 		}
 	}
+
 	// Int 和 String 可以帮助我们分别取出 reflect.Value 作为 int64 和 string
 	{
 		a := 56
@@ -91,12 +211,6 @@ func TStruct(s interface{}) {
 
 	// 获取方法， 方法必须是可导出的
 	{
-		fmt.Println(reflect.TypeOf(s).NumMethod(), reflect.TypeOf(s).Name())
-		for i := 0; i < reflect.TypeOf(s).NumMethod(); i++ {
-			m := t.Method(i)
-			fmt.Println("=======", m.Name)
-		}
-
 		m := v.MethodByName("Hello")
 		/*
 			这里会 panic，因为上面将传入的 v 被 剥离了指针
@@ -108,8 +222,27 @@ func TStruct(s interface{}) {
 	}
 }
 
-// 通过反射调用结构体的方法
-func main1() {
+// TStructModifyValue 通过反射修改值
+func TStructModifyValue() {
+
+	var a int = 1024
+	rValue := reflect.ValueOf(&a) // 获取变量a的反射值对象
+	rValue = rValue.Elem()        // 取出a地址的元素(a的值)
+	rValue.SetInt(1)              // 尝试将a修改为1
+	fmt.Println(rValue.Int())
+
+	type dog struct {
+		legCount int
+	}
+
+	valueOfDog := reflect.ValueOf(&dog{}) // 获取dog实例的反射值对象
+	valueOfDog = valueOfDog.Elem()
+	vLegCount := valueOfDog.FieldByName("legCount") // 获取legCount字段的值
+	vLegCount.SetInt(4)                             //尝试设置legCount的值(这里会发生崩溃), 原因是 legCount 不能被导出
+}
+
+// TStructCall 通过反射调用结构体的方法
+func TStructCall() {
 	//通过反射的方式调用结构体类型的方法
 	var setNameStr string = "SetName"
 	var addAgeStr string = "AddAge"
@@ -120,7 +253,7 @@ func main1() {
 	}
 
 	//1.获取到结构体类型变量的反射类型
-	refUser := reflect.ValueOf(&user) //需要传入指针，后面再解析
+	refUser := reflect.ValueOf(&user) // 需要传入指针，后面再解析
 	fmt.Println(refUser)
 
 	//2.获取确切的方法名
@@ -128,6 +261,7 @@ func main1() {
 	setNameMethod := refUser.MethodByName(setNameStr)
 	args := []reflect.Value{reflect.ValueOf("Mike")} //构造一个类型为reflect.Value的切片
 	setNameMethod.Call(args)                         //返回Value类型
+
 	//不带参数调用方式
 	addAgeMethod := refUser.MethodByName(addAgeStr)
 	addAgeMethod.Call(make([]reflect.Value, 0))
@@ -135,6 +269,17 @@ func main1() {
 	fmt.Println("User.Name = ", user.Name)
 	fmt.Println("User.Age = ", user.Age)
 
+	{
+		var myMath = MyMath{Pi: 3.14159}
+		rValue := reflect.ValueOf(myMath)                                      // 获取myMath的值对象
+		paramList := []reflect.Value{reflect.ValueOf(30), reflect.ValueOf(20)} // 构造函数参数，传入两个整形值
+
+		//调用结构体的第一个方法Method(0)
+		//注意:在反射值对象中方法索引的顺序并不是结构体方法定义的先后顺序
+		//而是根据方法的ASCII码值来从小到大排序，所以Dec排在第一个，也就是Method(0)
+		result := rValue.Method(0).Call(paramList)
+		fmt.Println(result[0].Int())
+	}
 }
 
 type UserInfo struct {
@@ -164,4 +309,17 @@ func (u *UserInfo) AddAge() bool {
 //测试方法
 func (u UserInfo) TestUser() {
 	fmt.Println("我只是输出某些内容而已....")
+}
+
+type MyMath struct {
+	Pi float64
+}
+
+//普通函数
+func (myMath MyMath) Sum(a, b int) int {
+	return a + b
+}
+
+func (myMath MyMath) Dec(a, b int) int {
+	return a - b
 }
