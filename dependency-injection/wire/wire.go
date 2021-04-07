540c41a7b17b4974a8495dc44efc7d69a932ddce
majunjun12345 <598959594@qq.com>
2021-04-07 15:55:32 +0800

    fix: inject

--- dependency-injection/wire/wire.go
+++ dependency-injection/wire/wire.go
@@ -15,6 +15,13 @@ import "github.com/google/wire"
 	如果我们想要定制创建行为，就需要为不同类型创建不同的参数结构：
 
 	error: NewPlayer 的 error 作为 InitMission 的返回值
+
+
+	直接在当前目录下执行 wire 或者 新建 wire_gen.go //go:generate wire package main 后再执行 go generate
+
+	go generate
+	当运行go generate时，它将扫描与当前包相关的源代码文件，找出所有包含"//go:generate"的特殊注释，
+	提取并执行该特殊注释后面的命令，命令为可执行程序，形同shell下面执行。
 */
 func InitMission(player PlayerParam, monster MonsterParam) (*Mission, error) {
 	wire.Build(NewMonster, NewPlayer, NewMission)
